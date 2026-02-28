23cc5e37ba791a857df3e5101fa58054fc4e07c7
anaoanao1 <yufei8028@gmail.com>
2026-02-28 12:49:01 -0800

    jlkj

--- slime.gd
+++ slime.gd
@@ -2,9 +2,14 @@ extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var speed = 75
+var player = null
+
 @export var direction = -1 #1 is right, -1 is left
+@export var follow_distance = 600
 
 func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	print("PLAYER =", player)
 	$AnimatedSprite2D.flip_h = false if direction == -1 else true
 	
 func _physics_process(delta):
@@ -12,11 +17,16 @@ func _physics_process(delta):
 	#fall with gravity
 	velocity.y += gravity * delta
 	
-	#turn around on walls
-	if is_on_wall():
-		direction = direction * -1
-		$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
-	
+	# Follow player if close enough
+	if player:
+		var distance = global_position.distance_to(player.global_position)
+		
+		if distance < follow_distance:
+			direction = sign(player.global_position.x - global_position.x)
+		else: 
+			if is_on_wall():
+				direction = direction * -1
+				$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
 	
 	#move left/right
 	velocity.x = speed * direction
@@ -24,9 +34,6 @@ func _physics_process(delta):
 	#actually move the object
 	move_and_slide()
 	
-
-
-
 func _on_hurt_zone_body_entered(body):
 	body.die()
 
