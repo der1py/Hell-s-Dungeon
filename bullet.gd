f62484d52b0101b200adb0a1abbc7dc5a90a812e
der1py <lukezf2@gmail.com>
2026-03-01 18:19:38 -0800

    sdsdhsuaduashd

--- bullet.gd
+++ bullet.gd
@@ -4,15 +4,21 @@ extends Area2D
 var speed := 900
 var direction: Vector2
 var damage = 10
+var start_position: Vector2
+var range = 5000
 
 func _ready():
 	body_entered.connect(_on_hurt_zone_body_entered)
+	start_position = global_position
 
 func _physics_process(delta):
 	direction = Vector2.RIGHT.rotated(rotation)
 	global_position += direction * speed * delta
 	# if not get_viewport_rect().has_point(global_position):
 	# 	queue_free() # ?
+
+	if global_position.distance_to(start_position) >= range:
+		queue_free()
 
 func _on_hurt_zone_body_entered(body):
 	if body.is_in_group("player") and body.has_method("take_damage"):
